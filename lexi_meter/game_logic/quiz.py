7a451099852f31810f52974c8a09c057515447a1
Lucas Meyer <lucas@praelexis.com>
2024-08-08 13:03:56 +0200

    Fix tests. Add backend class for quiz (WIP).

--- lexi_meter/game_logic/quiz.py
+++
@@ -1,8 +0,0 @@
-class Quiz:
-    """
-    The Quiz class defines the main game (or quiz) logic for the backend
-    of the application.
-    """
-
-    def __init__(self):
-        self.questions = []
